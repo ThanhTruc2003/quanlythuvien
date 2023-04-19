3b55b92bd96c7dbc2c5c51638b232689deb31f55
Thanh Nguyen <thanh.nguyen@beetech.com.vn>
2023-04-19 11:05:13 +0700

    complete create book

---
+++ ThuVien/BUS/CreateBookBUS.cs
@@ -0,0 +1,15 @@
+﻿using DTO;
+using DAL;
+using System.Data;
+
+namespace BUS
+{
+    public class CreateBookBUS
+    {
+        private CreateBookDAL dal = new CreateBookDAL();
+        public bool Insert(CreateBookDTO dto_book)
+        {
+            return dal.Insert(dto_book);
+        }
+    }
+}
---
+++ ThuVien/GUI/CreateBook.cs
@@ -0,0 +1,42 @@
+﻿using BUS;
+using DTO;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThuVien.GUI
+{
+    public partial class CreateBook : Form
+    {
+        public CreateBook()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string TenSach, NhaXuatBan;
+            int SoLuong;
+           
+            TenSach = txbTenSach.Text;
+            NhaXuatBan = txbNhaXuatBan.Text;
+            SoLuong = Convert.ToInt32(txbSoLuong.Text);
+            CreateBookBUS bus = new CreateBookBUS();
+            bool isSuccess = bus.Insert(new DTO.CreateBookDTO(TenSach, NhaXuatBan, SoLuong, SoLuong));
+            if(isSuccess)
+            {
+                MessageBox.Show("Thêm thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txbTenSach.Text = "";
+                txbNhaXuatBan.Text = "";
+                txbSoLuong.Text = "";
+            }
+        }
+    }
+}
